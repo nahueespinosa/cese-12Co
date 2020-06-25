897be1958108517ef2ddc5ed8e7c90b877d3945d
Nahuel Espinosa <nahue.espinosa@gmail.com>
2020-06-25 20:44:37 -0300

    Agregar archivos base para ej2 de PdM

---
+++ pdm/ej2-clase1/inc/ej2-clase1.h
@@ -0,0 +1,40 @@
+/*=============================================================================
+ * Author: Nahuel Espinosa <nahue.espinosa@gmail.com>
+ * Date: 2020/06/25
+ *===========================================================================*/
+
+/*=====[Avoid multiple inclusion - begin]====================================*/
+
+#ifndef __EJ2_CLASE1_H__
+#define __EJ2_CLASE1_H__
+
+/*=====[Inclusions of public function dependencies]==========================*/
+
+#include <stdint.h>
+#include <stddef.h>
+
+/*=====[C++ - begin]=========================================================*/
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*=====[Definition macros of public constants]===============================*/
+
+/*=====[Public function-like macros]=========================================*/
+
+/*=====[Definitions of public data types]====================================*/
+
+/*=====[Prototypes (declarations) of public functions]=======================*/
+
+/*=====[Prototypes (declarations) of public interrupt functions]=============*/
+
+/*=====[C++ - end]===========================================================*/
+
+#ifdef __cplusplus
+}
+#endif
+
+/*=====[Avoid multiple inclusion - end]======================================*/
+
+#endif /* __EJ2_CLASE1_H__ */
---
+++ pdm/ej2-clase1/src/ej2-clase1.c
@@ -0,0 +1,36 @@
+/*=============================================================================
+ * Author: Nahuel Espinosa <nahue.espinosa@gmail.com>
+ * Date: 2020/06/25
+ *===========================================================================*/
+
+/*=====[Inclusions of function dependencies]=================================*/
+
+#include "ej2-clase1.h"
+#include "sapi.h"
+
+/*=====[Definition macros of private constants]==============================*/
+
+/*=====[Definitions of extern global variables]==============================*/
+
+/*=====[Definitions of public global variables]==============================*/
+
+/*=====[Definitions of private global variables]=============================*/
+
+/*=====[Main function, program entry point after power on or reset]==========*/
+
+int main( void )
+{
+   // ----- Setup -----------------------------------
+   boardInit();
+
+   // ----- Repeat for ever -------------------------
+   while( true ) {
+      gpioToggle(LED);
+      delay(500);
+   }
+
+   // YOU NEVER REACH HERE, because this program runs directly or on a
+   // microcontroller and is not called by any Operating System, as in the 
+   // case of a PC program.
+   return 0;
+}
