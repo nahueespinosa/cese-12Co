cdaccab7bc6d9e997e1cc5469e47c7545c2683af
Nahuel Espinosa <nahue.espinosa@gmail.com>
2020-10-27 21:53:08 -0300

    Actualizar práctica de SOPG

--- sopg/clase2/main.c
+++ sopg/clase2/main.c
@@ -1,14 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
-int main( void ) {
 
-    if( fork() == 0 ) {
-        printf("Hola desde el hijo!\n");
-        sleep(20);
-    } else {
-        printf("Hola desde el padre!\n");
-        sleep(40);
-    }
-    return 0;
+int main(void)
+{
+	pid_t pid;
+	switch(pid = fork())
+	{
+		case -1:
+            perror("fork"); /* something went wrong */
+            exit(1);
+            break;
+
+		/* parent exits */
+		case 0:
+            printf(" CHILD: This is the child process!\n");
+            printf(" CHILD: My PID is %d\n", getpid());
+            printf(" CHILD: My parent's PID is %d\n", getppid());
+            printf(" CHILD: Enter my exit status (make it small): ");
+            sleep(20);
+            printf(" CHILD: I'm outta here!\n");
+            exit(0);
+            break;
+
+
+		default:
+            printf("PARENT: This is the parent process!\n");
+            printf("PARENT: My PID is %d\n", getpid());
+            printf("PARENT: My childs PID is %d\n", pid);
+            sleep(40);
+            printf("PARENT: I'm outta here!\n");
+            break;
+	}
 }
