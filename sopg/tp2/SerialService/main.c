3695d32cc43446d829a3b5d7869883f1b3df76fa
Nahuel Espinosa <nahue.espinosa@gmail.com>
2020-12-03 16:23:04 -0300

    Mejoras en la implementación

--- sopg/tp2/SerialService/main.c
+++ sopg/tp2/SerialService/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <stdlib.h>
@@ -22,11 +23,9 @@
 #define SERIAL_PORT 1
 
 int s;
+bool close_from_signal = false;
 pthread_t tcp_service_thread;
 pthread_t serial_service_thread;
-
-/* msleep(): Sleep for the requested number of milliseconds. */
-int msleep(long msec);
 
 /* msleep(): Block signals for this thread. */
 void block_signals(void);
@@ -46,9 +45,10 @@ int main()
 	socklen_t addr_len;
 	struct sockaddr_in clientaddr;
 	struct sockaddr_in serveraddr;
+	char ip_client[32];
+	int main_status = EXIT_FAILURE;
 
 	/* Initialize signal handler */
-
 	struct sigaction sa;
 	sa.sa_handler = sig_handler;
 	sa.sa_flags = 0;
@@ -56,20 +56,26 @@ int main()
 
 	if (sigaction(SIGINT, &sa, NULL) < 0)
 	{
-		perror("sigaction() error");
-		exit(1);
+		perror("sigaction(SIGINT) error");
+		exit(EXIT_FAILURE);
 	}
 
 	if (sigaction(SIGTERM, &sa, NULL) < 0)
 	{
-		perror("sigaction() error");
-		exit(1);
+		perror("sigaction(SIGTERM) error");
+		exit(EXIT_FAILURE);
 	}
 
-	signal(SIGPIPE, SIG_IGN);
+	/* Ignore sigpipe to handle the client disconnect in the main loop */
+	sa.sa_handler = SIG_IGN;
+
+	if (sigaction(SIGPIPE, &sa, NULL) < 0)
+	{
+		perror("sigaction(SIGPIPE) error");
+		exit(EXIT_FAILURE);
+	}
 
 	/* Initialize server IP address */
-
 	bzero((char *)&serveraddr, sizeof(serveraddr));
 	serveraddr.sin_family = AF_INET;
 	serveraddr.sin_port = htons(TCP_PORT);
@@ -77,41 +83,53 @@ int main()
 	if (inet_pton(AF_INET, SERVER_IP, &(serveraddr.sin_addr)) <= 0)
 	{
 		perror("invalid server IP\n");
-		exit(1);
+		exit(EXIT_FAILURE);
 	}
 
 	/* Create TCP socket */
-
 	s = socket(AF_INET, SOCK_STREAM, 0);
+
+	if (s < 0)
+	{
+		perror("socket() error");
+		exit(EXIT_FAILURE);
+	}
 
 	if (bind(s, (struct sockaddr *)&serveraddr, sizeof(serveraddr)) < 0)
 	{
 		close(s);
 		perror("bind() error");
-		exit(1);
+		exit(EXIT_FAILURE);
 	}
 
 	if (listen(s, 10) < 0)
 	{
 		close(s);
 		perror("listen() error");
-		exit(1);
+		exit(EXIT_FAILURE);
 	}
 
-	while (1)
+	while (true)
 	{
 		printf("Esperando conexion desde InterfaceService\n");
 
 		addr_len = sizeof(struct sockaddr_in);
 		if ((fd = accept(s, (struct sockaddr *)&clientaddr, &addr_len)) < 0)
 		{
-			perror("accept() error");
+			/* Check if the user is trying to close the program */
+			if (errno == EINTR && close_from_signal == true)
+			{
+				main_status = EXIT_SUCCESS;
+			}
+			else
+			{
+				perror("accept() error");
+			}
 			break;
 		}
 
-		char ipClient[32];
-		inet_ntop(AF_INET, &(clientaddr.sin_addr), ipClient, sizeof(ipClient));
-		printf("Nueva conexion desde %s\n", ipClient);
+		inet_ntop(AF_INET, &(clientaddr.sin_addr), ip_client, sizeof(ip_client));
+		printf("Nueva conexion desde %s\n", ip_client);
 
 		printf("Conectando con la EDU-CIAA\n");
 
@@ -121,34 +139,59 @@ int main()
 			break;
 		}
 
-		block_signals();	// Only the main thread handles the signals
+		block_signals(); // Only the main thread handles the signals
 
 		if (pthread_create(&tcp_service_thread, NULL, tcp_service_routine, &fd) != 0)
 		{
-			perror("pthread_create() error");
+			perror("pthread_create(&tcp_service_thread) error");
 			break;
 		}
 
 		if (pthread_create(&serial_service_thread, NULL, serial_service_routine, &fd) != 0)
 		{
-			perror("pthread_create() error");
+			perror("pthread_create(&serial_service_thread) error");
 			break;
 		}
 
-		unblock_signals();
+		unblock_signals(); // Only the main thread handles the signals
 
 		printf("Esperando comandos\n");
 
-		pthread_join(tcp_service_thread, NULL);
-		pthread_cancel(serial_service_thread);
-
-		serial_close();
+		if (pthread_join(tcp_service_thread, NULL) < 0)
+		{
+			perror("pthread_join() error");
+			break;
+		}
 
 		printf("\nCliente desconectado\n");
+
+		pthread_cancel(serial_service_thread);
+
+		if (pthread_join(serial_service_thread, NULL) < 0)
+		{
+			perror("pthread_join() error");
+			break;
+		}
+
+		serial_close();
+		close(fd);
+
+		/* Check if the user is trying to close the program */
+		if (close_from_signal == true)
+		{
+			main_status = EXIT_SUCCESS;
+			break;
+		}
 	}
 
+	printf("\nCerrando el servidor\n");
+
+	/* Make sure everything is closed */
 	close(s);
-	exit(1);
+	close(fd);
+	serial_close();
+
+	return main_status;
 }
 
 void *tcp_service_routine(void *arg)
@@ -173,6 +216,8 @@ void *tcp_service_routine(void *arg)
 			break;
 		}
 	}
+
+	return (void *)0;
 }
 
 void *serial_service_routine(void *arg)
@@ -196,45 +241,22 @@ void *serial_service_routine(void *arg)
 			}
 		}
 
-		msleep(10);
+		usleep(10000);
 	}
+
+	return (void *)0;
 }
 
 void sig_handler(int sig)
 {
 	if (sig == SIGTERM || sig == SIGINT)
 	{
-		write(1, "\nCerrando el servidor\n", 22);
-
+		/* Try to cancel TCP thread if it is running */
 		pthread_cancel(tcp_service_thread);
-		pthread_cancel(serial_service_thread);
 
-		serial_close();
-		close(s);
-		exit(0);
+		/* Flag to close the program */
+		close_from_signal = true;
 	}
-}
-
-int msleep(long msec)
-{
-	struct timespec ts;
-	int res;
-
-	if (msec < 0)
-	{
-		errno = EINVAL;
-		return -1;
-	}
-
-	ts.tv_sec = msec / 1000;
-	ts.tv_nsec = (msec % 1000) * 1000000;
-
-	do
-	{
-		res = nanosleep(&ts, &ts);
-	} while (res && errno == EINTR);
-
-	return res;
 }
 
 void block_signals(void)
